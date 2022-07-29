6e57b99e46f7d24c1ca0234b593c9d2ba57c44c0
MisterCruz <ID+username@users.noreply.github.com>
2022-07-29 09:02:08 -0400

    adjust padding for timer clock

--- src/Pom.js
+++ src/Pom.js
@@ -32,7 +32,7 @@ class Pom extends Component {
         return (
             <div className="bg-slate-500">
                 <h1 className="text-3xl font-bold py-10 text-white"> Pomodoro Timer </h1>
-                {this.state.isStarted ? <p className="text-3xl font-bold py-4 text-white"> {this.state.minutes} : {this.state.seconds}</p> : null}
+                {this.state.isStarted ? <p className="text-3xl font-bold pt-2 pb-8 text-white"> {this.state.minutes} : {this.state.seconds}</p> : null}
                 <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded" onClick={this.countDown}>Start timer</button>
             </div>
         )
